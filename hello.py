f82796837c57b57165982a785b5ea63adb347cc0
a-srao <aishwaryasrao02@gmail.com>
2025-09-11 14:36:00 +0530

    Modified

---
+++ hello.py
@@ -0,0 +1,2 @@
+def greet_admin(name):
+    print(f"Welcome back, Admin {name}!")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from utils import greet_user
-
+from util import greet_user
+from hellos import greet_admin
 def main():
     
     user_name="World"
     print(greet_user(user_name))
+    admin_name="Alice"
+    greet_admin(admin_name)
 
 if __name__ == "__main__": \
     main()
